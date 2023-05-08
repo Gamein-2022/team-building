95fac653c0e5338cfd243bd497d4a371dc06b2dd
mohamadreza <mohamadrza.pr@gmail.com>
2023-05-08 22:59:46 +0330

    initial commit

---
+++ src/test/java/gamein2/team/BuildingApplicationTests.java
@@ -0,0 +1,13 @@
+package gamein2.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
