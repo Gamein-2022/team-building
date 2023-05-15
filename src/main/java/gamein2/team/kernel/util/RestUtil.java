1e068939363f0f3834685dd23e95e51e9d44b8d5
mohamadreza <mohamadrza.pr@gmail.com>
2023-05-15 11:41:13 +0330

    fix forgot password problem

--- src/main/java/gamein2/team/kernel/util/RestUtil.java
+++ src/main/java/gamein2/team/kernel/util/RestUtil.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestUtil {
     public static void sendSMS(String baseUrl, String token, String receptor, String template) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
-        String url = String.format(baseUrl + "?receptor=%s&token=$s&template=$s",receptor,token,template);
+        String url = String.format(baseUrl + "?receptor=%s&token=%s&template=%s",receptor,token,template);
         ResponseEntity<String> response = restTemplate.getForEntity(url,String.class);
         if (!response.getStatusCode().equals(HttpStatus.OK)){
             throw new Exception("خطا در ارسال پبامک");
