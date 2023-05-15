a29c2582db379b5ee2487151b066a7d65da0e161
mohamadreza <mohamadrza.pr@gmail.com>
2023-05-15 15:30:42 +0330

    fix createing team problem

--- src/main/java/gamein2/team/kernel/entity/Team.java
+++ src/main/java/gamein2/team/kernel/entity/Team.java
@@ -18,6 +18,7 @@ import java.util.List;
 public class Team {
     @Id
     @Column(name = "id", unique = true, nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToMany(fetch = FetchType.EAGER)
